eed5d5c3dbf3e26a80326e8857175dd5576d810b
leticia-lemos <leticiasilveira189@gmail.com>
2024-07-11 11:51:10 -0300

    nao aguento mais

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
-
 function mudarCorAzul() {
+  var accordion = document.getElementById("accordion-header");
+  var accordionText = document.getElementById("internos");
 
-    var accordion = document.getElementById("accordion-header");
-    var accordionText = document.getElementById("internos")
+  if (accordion.style.backgroundColor == "#2a55c2") {
+    accordion.style.backgroundColor = "#f8f8ff";
+    accordionText.style.color = "#2a55c2";
+  }
 
-     if (accordion.style.backgroundColor == "#2a55c2") {
-        accordion.style.backgroundColor = "#f8f8ff";
-        accordionText.style.color = "#2a55c2"
-    }
-
-
-    if (accordion.style.backgroundColor == "#f8f8ff") {
-        accordion.style.backgroundColor = "#2a55c2";
-        accordionText.style.color = "#f8f8ff"
-        }
-
-        
-    
+  if (accordion.style.backgroundColor == "#f8f8ff") {
+    accordion.style.backgroundColor = "#2a55c2";
+    accordionText.style.color = "#f8f8ff";
+  }
 }
-
- 

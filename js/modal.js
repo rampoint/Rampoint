5480ae6bb169dad40ea0d48dd5d3c2c615332674
Blackout <sergiogabriel2606g@outlook.com>
2024-07-23 01:10:10 -0300

    colovando qr

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,20 @@
 const switchModal = () => {
-  const modal = document.querySelector(".modal-sem-perfil");
-  const actualStyle = modal.style.display;
-  if (actualStyle == "block") {
-    modal.style.display = "none";
-  } else {
-    modal.style.display = "block";
+    const modal = document.querySelector('.modal-sem-perfil')
+    const actualStyle = modal.style.display
+    if(actualStyle == 'block') {
+      modal.style.display = 'none'
+    }
+    else {
+      modal.style.display = 'block'
+    }
   }
-};
-
-const btn = document.querySelector(".modalBtn");
-btn.addEventListener("click", switchModal);
-
-window.onclick = function (event) {
-  const modal = document.querySelector(".modal-sem-perfil");
-  if (event.target == modal) {
-    switchModal();
+  
+  const btn = document.querySelector('.modalBtn')
+  btn.addEventListener('click', switchModal)
+  
+  window.onclick = function(event) {
+      const modal = document.querySelector('.modal-sem-perfil')
+    if (event.target == modal) {
+      switchModal()
+    }
   }
-};

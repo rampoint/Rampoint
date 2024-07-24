d4e459456630d2dba6dcb2cc1710937737a5b7d7
leticia-lemos <leticiasilveira189@gmail.com>
2024-07-24 07:38:39 -0300

    estrutura

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,19 @@
 const switchModal = () => {
-    const modal = document.querySelector('.modal-sem-perfil')
-    const actualStyle = modal.style.display
-    if(actualStyle == 'block') {
-      modal.style.display = 'none'
-    }
-    else {
-      modal.style.display = 'block'
-    }
+  const modal = document.querySelector(".modal-sem-perfil");
+  const actualStyle = modal.style.display;
+  if (actualStyle == "block") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "block";
   }
-  
-  const btn = document.querySelector('.modalBtn')
-  btn.addEventListener('click', switchModal)
-  
-  window.onclick = function(event) {
-      const modal = document.querySelector('.modal-sem-perfil')
-    if (event.target == modal) {
-      switchModal()
-    }
+};
+
+const btn = document.querySelector(".modalBtn");
+btn.addEventListener("click", switchModal);
+
+window.onclick = function (event) {
+  const modal = document.querySelector(".modal-sem-perfil");
+  if (event.target == modal) {
+    switchModal();
   }
+};

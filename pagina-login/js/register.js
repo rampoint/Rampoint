b38597be435aed81f8cc37f4880714704cd74653
Blackout <sergiogabriel2606g@outlook.com>
2024-08-20 17:09:03 -0300

    ja volto

--- pagina-login/js/register.js
+++ pagina-login/js/register.js
@@ -1,53 +1,50 @@
+function onchangeEmail() {
+  const email = Form.email().value;
+  Form.erro_email_obrigatorio().style.display = email ? "none" : "block";
 
-
-function onchangeEmail(){
-    const email = Form.email().value
-    Form.erro_email_obrigatorio().style.display = email ? "none" : "block";
-
-    Form.erro_email_register().style.display = validarEmail(email) ? "none": "block";
-    
+  Form.erro_email_register().style.display = validarEmail(email)
+    ? "none"
+    : "block";
 }
 
-function onchangePassword(){
-  const senha = Form.senha().value
+function onchangePassword() {
+  const senha = Form.senha().value;
   Form.erro_senha_register().style.display = senha ? "none" : "block";
 
   Form.erro_senha_min().style.display = senha.length >= 6 ? "none" : "block";
 
-  validarSenhaCorresponde()
+  validarSenhaCorresponde();
 }
 
-function onchangePasswordConfirm(){
-  validarSenhaCorresponde()
+function onchangePasswordConfirm() {
+  validarSenhaCorresponde();
 }
 
-function validarSenhaCorresponde(){
-  const senha = Form.senha().value
-  const confirmarSenha = Form.confirmarSenha().value
+function validarSenhaCorresponde() {
+  const senha = Form.senha().value;
+  const confirmarSenha = Form.confirmarSenha().value;
 
-    Form.erro_nao_corresponde().style.display = 
-  senha == confirmarSenha ? "none" : "block"
+  Form.erro_nao_corresponde().style.display =
+    senha == confirmarSenha ? "none" : "block";
 }
 
-function botaoDesativar(){
-  Form.cadastrar_button().disable = !isFormValid()
+function botaoDesativar() {
+  Form.cadastrar_button().disable = !isFormValid();
 }
 
-function isFormValid(){
+function isFormValid() {
+   const email = Form.email().value
+   if (!email || !ValidarEmail()){return false;}
+  const senha = Form.senha().value;
+  if (!senha || senha.length <= 5) {return false;}
+   const confirmarSenha = Form.confirmarSenha().value
+    if (senha != confirmarSenha){return false;}
 
-  const email = Form.email().value
-  if (!email || !ValidarEmail()){return false}
-  // const senha = Form.senha().value
-  // if (!senha || senha.length <= 6){return false}
-  const confirmarSenha = Form.confirmarSenha().value
-   if (senha != confirmarSenha){return false} 
-
-
-  return true
+  return true;
 }
 
 function ValidarEmail() {
-  return /\S+@\S+\.\S+/.test(Form.email().value)
+  return /\S+@\S+\.\S+/.test(Form.email().value);
 }
 const Form = {
   email: () => document.getElementById("email-cadastro"),
@@ -56,12 +53,11 @@ const Form = {
   telefone: () => document.getElementById("telefone"),
   data: () => document.getElementById("data"),
   erro_email_register: () => document.getElementById("erro-email-register"),
-  erro_data: () => document.getElementById('erro-data'),
-  erro_email_obrigatorio: () => document.getElementById("erro-email-obrigatorio"),
-  erro_senha_min:() => document.getElementById("erro-senha-min"),
+  erro_data: () => document.getElementById("erro-data"),
+  erro_email_obrigatorio: () =>document.getElementById("erro-email-obrigatorio"),
+  erro_senha_min: () => document.getElementById("erro-senha-min"),
   erro_senha_register: () => document.getElementById("erro-senha-register"),
-  erro_senha_obrigatorio: () => document.getElementById("erro-senha-obrigatorio"),
+  erro_senha_obrigatorio: () =>document.getElementById("erro-senha-obrigatorio"),
   erro_nao_corresponde: () => document.getElementById("erro-nao-corresponde"),
-  cadastrar_button:() => document.getElementById("cadastrar")
-  
+  cadastrar_button: () => document.getElementById("cadastrar"),
 };

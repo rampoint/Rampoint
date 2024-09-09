0a161298fc018e0eac95123da08d6262ca450c71
Leticia Lemos <leticiasilveira189@gmail.com>
2024-09-08 22:34:01 -0300

    começo da desgraça

---
+++ notificacoes-adm/js/mensagens.js
@@ -0,0 +1,8 @@
+// Função para exibir os detalhes da mensagem
+function showDetails(index) {
+  const details = document.getElementById("mensagem");
+
+  if (index === 0) {
+    details.style.display = "block"; // Exibe o conteúdo da primeira mensagem
+  }
+}

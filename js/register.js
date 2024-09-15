9536f6e1189f048139d694fcbc4b55ee9df2d0b1
Blackout <sergiogabriel2606g@outlook.com>
2024-09-15 13:51:11 -0300

    FEITOOOO

--- js/register.js
+++ js/register.js
@@ -10,10 +10,12 @@ const Form = {
   data: () => document.getElementById("data-cadastro"),
   erro_email_register: () => document.getElementById("erro-email-register"),
   erro_data: () => document.getElementById("erro-data"),
-  erro_email_obrigatorio: () => document.getElementById("erro-email-obrigatorio"),
+  erro_email_obrigatorio: () =>
+    document.getElementById("erro-email-obrigatorio"),
   erro_senha_min: () => document.getElementById("erro-senha-min"),
   erro_senha_register: () => document.getElementById("erro-senha-register"),
-  erro_senha_obrigatorio: () => document.getElementById("erro-senha-obrigatorio"),
+  erro_senha_obrigatorio: () =>
+    document.getElementById("erro-senha-obrigatorio"),
   erro_nao_corresponde: () => document.getElementById("erro-nao-corresponde"),
   cadastrar_button: () => document.getElementById("cadastrar"),
 };
@@ -23,7 +25,7 @@ const form_usuario = {
   email_perfil: () => document.getElementById("email_usuario"),
   emocao_usuario: () => document.getElementById("emocao_usuario"),
   pontos: () => document.getElementById("pontos_usuario"),
-  foto_perfil: () => document.getElementById("foto-usuario")
+  foto_perfil: () => document.getElementById("foto-usuario"),
 };
 
 function mudandoBotao(mensagem) {
@@ -47,7 +49,10 @@ function calcularIdade(dataNascimento) {
   const mesAtual = hoje.getMonth();
   const mesNascimento = dataNascimento.getMonth();
 
-  if (mesAtual < mesNascimento || (mesAtual === mesNascimento && hoje.getDate() < dataNascimento.getDate())) {
+  if (
+    mesAtual < mesNascimento ||
+    (mesAtual === mesNascimento && hoje.getDate() < dataNascimento.getDate())
+  ) {
     idade--;
   }
   return idade;
@@ -58,7 +63,9 @@ function register() {
   const email = Form.email().value;
   const senha = Form.senha().value;
 
-  firebase.auth().createUserWithEmailAndPassword(email, senha)
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, senha)
     .then((userCredential) => {
       mudandoBotao("Cadastrando...");
       let uid = userCredential.user.uid;
@@ -81,27 +88,30 @@ function saveUserData(uid, email) {
   const database = firebase.database();
   const userRef = database.ref("users/" + uid);
 
-  userRef.set({
-    email: email,
-    nome: nome,
-    data: dataNascimento,
-    tel: telefone,
-    genero: genero,
-    createdAt: firebase.database.ServerValue.TIMESTAMP,
-  })
-  .then(() => {
-    console.log("Dados do usuário salvos com sucesso!");
-  })
-  .catch((error) => {
-    console.error("Erro ao salvar dados do usuário:", error);
-  });
+  userRef
+    .set({
+      email: email,
+      nome: nome,
+      data: dataNascimento,
+      tel: telefone,
+      genero: genero,
+      createdAt: firebase.database.ServerValue.TIMESTAMP,
+    })
+    .then(() => {
+      console.log("Dados do usuário salvos com sucesso!");
+    })
+    .catch((error) => {
+      console.error("Erro ao salvar dados do usuário:", error);
+    });
 }
 
 // Form Validation
 function onchangeEmail() {
   const email = Form.email().value;
   Form.erro_email_obrigatorio().style.display = email ? "none" : "block";
-  Form.erro_email_register().style.display = validarEmail_cadastro(email) ? "none" : "block";
+  Form.erro_email_register().style.display = validarEmail_cadastro(email)
+    ? "none"
+    : "block";
   botaoDesativar();
 }
 
@@ -121,7 +131,8 @@ function onchangePasswordConfirm() {
 function validarSenhaCorresponde() {
   const senha = Form.senha().value;
   const confirmarSenha = Form.confirmarSenha().value;
-  Form.erro_nao_corresponde().style.display = senha === confirmarSenha ? "none" : "block";
+  Form.erro_nao_corresponde().style.display =
+    senha === confirmarSenha ? "none" : "block";
 }
 
 function botaoDesativar() {
@@ -169,9 +180,10 @@ firebase.auth().onAuthStateChanged((user) => {
 });
 
 function buscarDadosUsuario(uid) {
-  const dbRef = firebase.database().ref('users/' + uid);
+  const dbRef = firebase.database().ref("users/" + uid);
 
-  dbRef.once('value')
+  dbRef
+    .once("value")
     .then((snapshot) => {
       if (snapshot.exists()) {
         const dadosUsuario = snapshot.val();
@@ -184,60 +196,87 @@ function buscarDadosUsuario(uid) {
     });
 }
 
-
-
-
 function exibirDadosUsuario(users) {
   // Obtém a URL atual da página
-var currentPage = window.location.href;
+  var currentPage = window.location.href;
 
-// Verifica se a URL contém uma string específica
-if (currentPage.includes("/pagina-inicial/inicial.html")) {
-  document.getElementById("nome_modal").innerHTML = users.nome
-} else if (currentPage.includes("/pagina-perfil/")) {
-  form_usuario.nome_perfil().innerHTML = users.nome;
-  form_usuario.email_perfil().innerHTML = users.email;
-  document.getElementById('nome-mudar').placeholder = users.nome;
-  document.getElementById('email-mudar').placeholder = users.email;
-  document.getElementById('telefone-mudar').placeholder = users.tel;
-  document.getElementById('nome-mudar').value = users.nome;
-  document.getElementById('email-mudar').value = users.email;
-  document.getElementById('telefone-mudar').value = users.tel;
-  document.getElementById('medalha-azul').style.display = users.medalhas.azul.display
-  document.getElementById('medalha-azul-img').src = users.medalhas.azul.img
+  // Verifica se a URL contém uma string específica
+  if (currentPage.includes("/pagina-inicial/inicial.html")) {
+    document.getElementById("nome_modal").innerHTML = users.nome;
+  } else if (currentPage.includes("/pagina-perfil/")) {
+    form_usuario.nome_perfil().innerHTML = users.nome;
+    form_usuario.email_perfil().innerHTML = users.email;
+    document.getElementById("nome-mudar").placeholder = users.nome;
+    document.getElementById("email-mudar").placeholder = users.email;
+    document.getElementById("telefone-mudar").placeholder = users.tel;
+    document.getElementById("nome-mudar").value = users.nome;
+    document.getElementById("email-mudar").value = users.email;
+    document.getElementById("telefone-mudar").value = users.tel;
+    document.getElementById("medalha-azul").style.display =
+      users.medalhas.azul.display;
+    document.getElementById("medalha-azul-img").src = users.medalhas.azul.img;
+    document.getElementById("nome_modal").innerHTML = users.nome;
+  } else {
+    console.log("O usuário está em outra página");
+  }
 }
- else {
-  console.log("O usuário está em outra página");
-}}
 
 function mudarDados() {
-  const nome_mudanca = document.getElementById('nome-mudar').value;
-  const telefone_mudanca = document.getElementById('telefone-mudar').value;
-  const genero_mudanca = document.getElementById('genero-mudar').value;
+  const nome_mudanca = document.getElementById("nome-mudar").value;
+  const telefone_mudanca = document.getElementById("telefone-mudar").value;
+  const genero_mudanca = document.getElementById("genero-mudar").value;
   const userRef = firebase.database().ref("users/" + globalUserId);
-    userRef.update({ 
-      nome: nome_mudanca ,
+  userRef
+    .update({
+      nome: nome_mudanca,
       tel: telefone_mudanca,
-      genero: genero_mudanca
+      genero: genero_mudanca,
     })
-      .then(() => {
-        adicionarMedalhas()
-        alert("Dados do usuário modificados com sucesso");
-      })
-  }
+    .then(() => {
+      adicionarMedalhas();
+      alert("Dados do usuário modificados com sucesso");
+    });
+}
 
-  function adicionarMedalhas(){
- 
-    const userRef = firebase.database().ref("users/" + globalUserId);
-    userRef.child('medalhas/azul').set({
-      nome:"Engrenagem Solidária",
-      display:"flex",
+function adicionarMedalhas() {
+  const userRef = firebase.database().ref("users/" + globalUserId);
+  userRef
+    .child("medalhas/azul")
+    .set({
+      nome: "Engrenagem Solidária",
+      display: "flex",
       descricao: "Personalizou perfil",
-      img:"https://firebasestorage.googleapis.com/v0/b/rampoint-81352.appspot.com/o/medalhas%2Fmedalha%20azul.svg?alt=media&token=c7134715-8121-44cb-9823-9a013761513d",
-    }).then(() =>{
-      console.log("enviado para o banco")
-    }).catch((error) =>{
-      console.log('nao foi esse o motivo' + error)
+      img: "https://firebasestorage.googleapis.com/v0/b/rampoint-81352.appspot.com/o/medalhas%2Fmedalha%20azul.svg?alt=media&token=c7134715-8121-44cb-9823-9a013761513d",
     })
+    .then(() => {
+      console.log("enviado para o banco");
+    })
+    .catch((error) => {
+      console.log("nao foi esse o motivo" + error);
+    });
+}
 
-  }
+function apagandoUsuario() {
+  const user = firebase.auth().currentUser;
+
+  user
+    .delete()
+    .then(() => {
+      alert('apagado filho')
+      logout()
+    })
+    .catch((error) => {
+      alert("nao apagado essa porra" + error.code)
+      // ...
+    });
+
+    const userRef = firebase.database().ref("users/" + globalUserId);
+
+    userRef.remove().
+    then(() =>{
+      alert('excluido do banco tambem slc')
+    })
+    .catch((error) => {
+      alert('receba ta errado')
+    })
+}

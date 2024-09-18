cbe0ba7f8fbd37ac1d515c97580b411a66ac32fe
BlackoutMomo <sergiogabriel2511g@gmail.com>
2024-09-18 09:59:41 -0300

    receba

---
+++ js/fotoDoperfil.js
@@ -0,0 +1,21 @@
+function pegarValorFotoFeliz(){
+    var ram = document.getElementById('ram-emocionado').value
+
+    console.log(ram)
+}
+
+function pegarValorFotoTriste(){
+    var ram = document.getElementById('ram-triste').value
+
+    console.log(ram)
+}
+function pegarValorFotoEntediado(){
+    var ram = document.getElementById('ram-entediado').value
+
+    console.log(ram)
+}
+function pegarValorFotoFurioso(){
+    var ram = document.getElementById('ram-furioso').value
+
+    console.log(ram)
+}

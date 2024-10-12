d5fd8caac4b1ec5b1def34fb51e5b4f3d03d3c2e
Blackout <sergiogabriel2606g@outlook.com>
2024-10-12 13:18:17 -0300

    excluir_funcionando

--- js/adm/encontrarDoacoes.js
+++ js/adm/encontrarDoacoes.js
@@ -1,42 +1,52 @@
 const formAdd = {
-    nome_peca: () => document.getElementById("nome-peca").innerHTML,
-    estrutura: () => document.getElementById("tipo-estrutura").innerHTML,
-    email_doador: () => document.getElementById("nome-doador").innerHTML,
-    nome_doador: () => document.getElementById("email-doador").innerHTML,
-    qtd: () => document.getElementById("email-doador").innerHTML,
-    vistoria: () => document.getElementById('status').innerHTML
+  nome_peca: () => document.getElementById("nome-peca").innerHTML,
+  estrutura: () => document.getElementById("tipo-estrutura").innerHTML,
+  email_doador: () => document.getElementById("nome-doador").innerHTML,
+  nome_doador: () => document.getElementById("email-doador").innerHTML,
+  qtd: () => document.getElementById("email-doador").innerHTML,
+  vistoria: () => document.getElementById("status").innerHTML,
+};
 
-}
+const usersRef = database.ref("users");
 
-const usersRef = database.ref('users')
+usersRef.once("value", (snapshot) => {
+  snapshot.forEach((childSnapshot) => {
+    var id = childSnapshot.key;
+    const usersChild = database.ref("users/" + id + "/peças");
 
-usersRef.once('value', (snapshot) => {
-    snapshot.forEach((childSnapshot) => {
-         
-        var id = childSnapshot.key;
-        const usersChild = database.ref('users/'+id+'/peças')
+    usersChild.once("value", (snapshot) => {
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          var nome_pecas = childSnapshot.key;
+          var dataPeca = childSnapshot.val();
 
-        usersChild.once('value', (snapshot) => {
-         if(snapshot.exists()){    
-            snapshot.forEach((childSnapshot) =>{
-                var nome_pecas = childSnapshot.key
-                var dataPeca = childSnapshot.val()
-                
-                criarTabela(dataPeca.nome_peca, dataPeca.tipo,dataPeca.email,dataPeca.nome,dataPeca.qtd,dataPeca.vistoria,childSnapshot.key)
-                
+          criarTabela(
+            dataPeca.nome_peca,
+            dataPeca.tipo,
+            dataPeca.email,
+            dataPeca.nome,
+            dataPeca.qtd,
+            dataPeca.vistoria,
+            childSnapshot.key
+          );
+        });
+      }
+    });
+  });
+});
 
-            }) 
-          
-         }
-        })
-        
-    })
-})
+function criarTabela(
+  nome_peca,
+  estrutura,
+  email_doador,
+  nome_doador,
+  qtd,
+  vistoria,
+  key
+) {
+  const novaLinha = document.createElement("tr");
 
-function criarTabela(nome_peca,estrutura,email_doador,nome_doador,qtd,vistoria){
-    const novaLinha = document.createElement('tr');
-    
-    novaLinha.innerHTML = `
+  novaLinha.innerHTML = `
         <td>
             <div class="peca">
                 <img src="./img/gabinete.svg" id="gabinete" alt="Imagem de um gabinete">
@@ -55,23 +65,24 @@ function criarTabela(nome_peca,estrutura,email_doador,nome_doador,qtd,vistoria){
                 ${vistoria}
             </button>
         </td>
-        <td>
-            <button class="opcoes" onclick="openModal('optionsModal')">
-                <img src="./img/more.svg" alt="Mais opções">
-            </button>
-
-            <!-- Modal Principal -->
-            <div id="optionsModal" class="modal">
-                <div class="modal-content">
-                    <ul>
-                        <li><button class="modal-button edit-button" onclick="editOption()"><img src="./img/icon-editar.svg" alt="">Editar</button></li>
-                        <li><button class="modal-button delete-button" onclick="openDeleteConfirmModal('deleteConfirmModal')"><img src="./img/icon-deletar.svg" alt="">Excluir</button></li>
-                    </ul>
-                </div>
-            </div>
+      <td>
+                <img src="img/excluir.png" id="excluir_editar" onclick="excluirDoacao('${key}')" alt="" srcset="">
+                <img src="img/editar.png" id="lapis_editar" alt="" srcset="">
         </td>`;
 
-    // Adiciona a nova linha ao corpo da tabela
-    document.getElementById('tbody').appendChild(novaLinha);
-
+  // Adiciona a nova linha ao corpo da tabela
+  document.getElementById("tbody").appendChild(novaLinha);
 }
+
+function excluirDoacao(id) {
+  usersRef.once("value", (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      var ids = childSnapshot.key;
+    database.ref('users/'+ids+'/peças/'+id).remove().then(() =>{
+        alert('doação removida com sucesso')
+    })
+})
+  })
+}
+
+      

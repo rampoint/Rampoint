14d883764c71fb6154bac802a2c6e5d4dd119995
Leticia Lemos <leticiasilveira189@gmail.com>
2024-10-19 13:15:22 -0300

    que botao dificil

---
+++ pagina-inicial/js/btn-acessibilidade.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const accessibilityBtn = document.getElementById("accessibilityBtn");
+  const accessibilityMenu = document.getElementById("accessibilityMenu");
+  const icon = accessibilityBtn.querySelector(".icon img");
+
+  const toggleMenu = () => {
+    const isMenuActive = accessibilityMenu.classList.toggle("active");
+
+    if (isMenuActive) {
+      accessibilityBtn.classList.add("menu-open");
+      icon.src = "./img/icon-close.svg";
+    } else {
+      accessibilityBtn.classList.remove("menu-open");
+      icon.src = "./img/icon-acessibilidade.svg";
+    }
+  };
+
+  accessibilityBtn.addEventListener("click", toggleMenu);
+
+  document.addEventListener("click", function (event) {
+    if (
+      !accessibilityBtn.contains(event.target) &&
+      !accessibilityMenu.contains(event.target)
+    ) {
+      if (accessibilityMenu.classList.contains("active")) {
+        toggleMenu();
+      }
+    }
+  });
+
+  const clearAccessibility = () => {
+    document.body.style.fontSize = "";
+    document.body.style.fontFamily = "";
+    document.body.style.lineHeight = "";
+    document.body.style.letterSpacing = "";
+    document.body.style.filter = "";
+  };
+
+  const clearBtn = document.getElementById("clearAccessibility");
+  clearBtn.addEventListener("click", clearAccessibility);
+});

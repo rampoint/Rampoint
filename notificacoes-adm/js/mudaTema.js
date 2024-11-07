5c540f684a6ac761f6ec2c4726b6413150a71938
Leticia Lemos <leticiasilveira189@gmail.com>
2024-11-07 00:44:38 -0300

    select funcionando

--- notificacoes-adm/js/mudaTema.js
+++ notificacoes-adm/js/mudaTema.js
@@ -16,53 +16,58 @@ document.addEventListener("DOMContentLoaded", () => {
   const setaMsg = document.querySelectorAll(".seta-mensagem i");
   const doadorMsg = document.querySelectorAll(".coluna-mensagem h3");
   const dataMsg = document.querySelectorAll(".data-hora");
+  const descricaoMsg = document.querySelectorAll(".descricao");
+  const conteudoDescricao = document.querySelectorAll(".conteudo-descricao");
+  const infoMsg = document.querySelector(".info h4");
 
   const aplicarTemaEscuro = () => {
     document.body.classList.add("dark-theme");
-
     mainContent.classList.add("dark-theme");
     sidebar.classList.add("dark-theme");
     spanLogo.classList.add("dark-theme");
     nameUser.classList.add("dark-theme");
-
-    iconSidebar.forEach((icon) => icon.classList.add("dark-theme"));
-    linkSidebar.forEach((link) => link.classList.add("dark-theme"));
+    iconSidebar.forEach(icon => icon.classList.add("dark-theme"));
+    linkSidebar.forEach(link => link.classList.add("dark-theme"));
 
     tituloPagina.classList.add("dark-theme");
     fundoTitulo.classList.add("dark-theme");
+
     titulo1.classList.add("dark-theme");
 
-    tituloMsg.forEach((title) => title.classList.add("dark-theme"));
-    setaMsg.forEach((arrow) => arrow.classList.add("dark-theme"));
-    doadorMsg.forEach((doador) => doador.classList.add("dark-theme"));
-    dataMsg.forEach((date) => date.classList.add("dark-theme"));
+    tituloMsg.forEach(title => title.classList.add("dark-theme"));
+    setaMsg.forEach(arrow => arrow.classList.add("dark-theme"));
+    doadorMsg.forEach(doador => doador.classList.add("dark-theme"));
+    dataMsg.forEach(date => date.classList.add("dark-theme"));
+    descricaoMsg.forEach(descricao => descricao.classList.add("dark-theme"));
+    conteudoDescricao.forEach(conteudo => conteudo.classList.add("dark-theme"));
+    infoMsg.classList.add("dark-theme");
 
-    themeIcon.classList.remove("bx-sun");
-    themeIcon.classList.add("bx-moon");
+    themeIcon.classList.replace("bx-sun", "bx-moon");
   };
 
   const aplicarTemaClaro = () => {
     document.body.classList.remove("dark-theme");
-
     mainContent.classList.remove("dark-theme");
     sidebar.classList.remove("dark-theme");
     spanLogo.classList.remove("dark-theme");
     nameUser.classList.remove("dark-theme");
-
-    iconSidebar.forEach((icon) => icon.classList.remove("dark-theme"));
-    linkSidebar.forEach((link) => link.classList.remove("dark-theme"));
+    iconSidebar.forEach(icon => icon.classList.remove("dark-theme"));
+    linkSidebar.forEach(link => link.classList.remove("dark-theme"));
 
     tituloPagina.classList.remove("dark-theme");
     fundoTitulo.classList.remove("dark-theme");
+
     titulo1.classList.remove("dark-theme");
 
-    tituloMsg.forEach((title) => title.classList.remove("dark-theme"));
-    setaMsg.forEach((arrow) => arrow.classList.remove("dark-theme"));
-    doadorMsg.forEach((doador) => doador.classList.remove("dark-theme"));
-    dataMsgMsg.forEach((date) => date.classList.remove("dark-theme"));
+    tituloMsg.forEach(title => title.classList.remove("dark-theme"));
+    setaMsg.forEach(arrow => arrow.classList.remove("dark-theme"));
+    doadorMsg.forEach(doador => doador.classList.remove("dark-theme"));
+    dataMsg.forEach(date => date.classList.remove("dark-theme"));
+    descricaoMsg.forEach(descricao => descricao.classList.remove("dark-theme"));
+    conteudoDescricao.forEach(conteudo => conteudo.classList.remove("dark-theme"));
+    infoMsg.classList.remove("dark-theme");
 
-    themeIcon.classList.add("bx-sun");
-    themeIcon.classList.remove("bx-moon");
+    themeIcon.classList.replace("bx-moon", "bx-sun");
   };
 
   if (localStorage.getItem("theme") === "dark") {

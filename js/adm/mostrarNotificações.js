a2ae75a64e55ef1331e31c595d6bd26bf9fb1e47
Leticia Lemos <leticiasilveira189@gmail.com>
2024-11-13 11:06:54 -0300

    arrumando sidebar

--- js/adm/mostrarNotificações.js
+++ js/adm/mostrarNotificações.js
@@ -1,30 +1,52 @@
-var usersRef = firebase.database().ref('users');
+var usersRef = firebase.database().ref("users");
 
-usersRef.once('value', (snapshot) => {
-    snapshot.forEach((childSnapshot) => {
-        var id = childSnapshot.key;
-        const usersChild = database.ref('users/' + id + '/peças');
+usersRef.once("value", (snapshot) => {
+  snapshot.forEach((childSnapshot) => {
+    var id = childSnapshot.key;
+    const usersChild = database.ref("users/" + id + "/peças");
 
-        usersChild.once('value', (snapshot) => {
-            if (snapshot.exists()) {
-                snapshot.forEach((childSnapshot) => {
-                    var nome_pecas = childSnapshot.key;
-                    var dataPeca = childSnapshot.val();
-                    criarTabela(nome_pecas, dataPeca.data, dataPeca.hora_doacao, dataPeca.desc, dataPeca.tipo, dataPeca.email, dataPeca.nome,dataPeca.nome_peca,dataPeca.qtd,dataPeca.nomeAdm);
-                });
-            }
+    usersChild.once("value", (snapshot) => {
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          var nome_pecas = childSnapshot.key;
+          var dataPeca = childSnapshot.val();
+          criarTabela(
+            nome_pecas,
+            dataPeca.data,
+            dataPeca.hora_doacao,
+            dataPeca.desc,
+            dataPeca.tipo,
+            dataPeca.email,
+            dataPeca.nome,
+            dataPeca.nome_peca,
+            dataPeca.qtd,
+            dataPeca.nomeAdm
+          );
         });
+      }
     });
+  });
 });
 
-function criarTabela(id, data, hora, desc, estrutura, email_doador, nome_doador, nome_peca, qtd,adm) {
-    console.log(adm)
-    const novaNotificacaoDesc = document.createElement('div');
-    novaNotificacaoDesc.classList.add('mensagem-detalhes');
-    novaNotificacaoDesc.id = `detalhes-${id}`;
-    novaNotificacaoDesc.style.display = 'none'; // Inicialmente escondido
+function criarTabela(
+  id,
+  data,
+  hora,
+  desc,
+  estrutura,
+  email_doador,
+  nome_doador,
+  nome_peca,
+  qtd,
+  adm
+) {
+  console.log(adm);
+  const novaNotificacaoDesc = document.createElement("div");
+  novaNotificacaoDesc.classList.add("mensagem-detalhes");
+  novaNotificacaoDesc.id = `detalhes-${id}`;
+  novaNotificacaoDesc.style.display = "none"; // Inicialmente escondido
 
-    novaNotificacaoDesc.innerHTML = `
+  novaNotificacaoDesc.innerHTML = `
         <div class="mensagem-header">
             <img src="./img/foto-adm.svg" alt="Foto de perfil do administrador">
             <div class="coluna-mensagem">
@@ -41,22 +63,22 @@ function criarTabela(id, data, hora, desc, estrutura, email_doador, nome_doador,
             <div class="info">
                 <h4>Informações pessoais</h4>
                 <p><span class="span-preto">Nome do doador:</span> ${nome_doador}</p>
-                <p><span class="span-preto">Email:</span>${email_doador}</p>
+                <p><span class="span-preto">Email:</span> ${email_doador}</p>
                 <p><span class="span-preto">Telefone:</span> teste</p>
             </div>
             <div class="info">
                 <h4>Informações da doação</h4>
-                <p><span class="span-preto">Peça:</span>${nome_peca}</p>
+                <p><span class="span-preto">Peça:</span> ${nome_peca}</p>
                 <p><span class="span-preto">Tipo de componente:</span> ${estrutura}</p>
-                <p><span class="span-preto">Quantidade:</span>${qtd}</p>
+                <p><span class="span-preto">Quantidade:</span> ${qtd}</p>
             </div>
         </div>
         <div class="btn-vistoria">
             <button onclick=editarDados('${id}') class="verify-button">Verificar</button>
         </div>`;
 
-    const novaNotificacao = document.createElement('div');
-    novaNotificacao.innerHTML = `
+  const novaNotificacao = document.createElement("div");
+  novaNotificacao.innerHTML = `
         <hr>
         <div class="mensagens-item" onclick="showDetails('${id}')">
             <div class="mensagem-info">
@@ -65,22 +87,22 @@ function criarTabela(id, data, hora, desc, estrutura, email_doador, nome_doador,
                 <p>${adm} adicionou uma nova doação</p>
             </div>
             <div class="seta-mensagem">
-                <button><img src="./img/seta-mensagem.svg" alt=""></button>
+                <button><i class='bx bx-chevron-right'></i></button>
             </div>
         </div>`;
 
-    document.getElementById('mensagens').appendChild(novaNotificacao);
-    document.getElementById('area-notificacao').appendChild(novaNotificacaoDesc);
+  document.getElementById("mensagens").appendChild(novaNotificacao);
+  document.getElementById("area-notificacao").appendChild(novaNotificacaoDesc);
 }
 
 function showDetails(id) {
-    // Esconde todos os detalhes
-    const allDetails = document.querySelectorAll('.mensagem-detalhes');
-    allDetails.forEach(detail => detail.style.display = 'none');
+  // Esconde todos os detalhes
+  const allDetails = document.querySelectorAll(".mensagem-detalhes");
+  allDetails.forEach((detail) => (detail.style.display = "none"));
 
-    // Exibe o detalhe correspondente
-    const detailToShow = document.getElementById(`detalhes-${id}`);
-    if (detailToShow) {
-        detailToShow.style.display = 'block';
-    }
+  // Exibe o detalhe correspondente
+  const detailToShow = document.getElementById(`detalhes-${id}`);
+  if (detailToShow) {
+    detailToShow.style.display = "block";
+  }
 }

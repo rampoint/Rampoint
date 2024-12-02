ea6544358f5969c5f84d639bab9a76dd3a673ad9
Blackout <sergiogabriel2606g@outlook.com>
2024-12-02 07:50:53 -0300

    segurança

--- js/logout.js
+++ js/logout.js
@@ -5,3 +5,11 @@ function logout(){
         alert("Erro ao fazer logout")
     })
 }
+
+document.getElementsByClassName('sair')[0].addEventListener('click', () => {
+    firebase.auth().signOut().then(() =>{
+        window.location.href = "../pagina-login/index.html"
+    }).catch(() =>{
+        alert("Erro ao fazer logout")
+    })
+})

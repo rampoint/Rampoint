1c3ae13daacc426b4bbfc30079dd45b62dcfbbe5
Blackout <sergiogabriel2606g@outlook.com>
2024-12-03 22:48:33 -0300

    apagarCupom

--- js/mostrarCupom.js
+++ js/mostrarCupom.js
@@ -13,21 +13,20 @@ function pegarCupons() {
               const data = childSnapshot.val();
               console.log(codigoCupom);
               console.log(data);
-              const nome_cupom = transformarString(data.nome_cupom)
+              const nome_cupom = transformarString(data.nome_cupom);
               var containerCupom = document.createElement("li");
               containerCupom.classList.add("conjunto-cupons");
+              containerCupom.setAttribute("data-cupom", codigoCupom); // Adiciona um atributo para identificar o cupom
               containerCupom.innerHTML = `<img src="./img/${data.nome_cupom}.svg" alt="">
                                             <div class="lista-textos2">
                                                 <p class="titulo-${data.nome_cupom}">CUPOM ${nome_cupom}</p>
                                                 <p class="porcentagem-${data.nome_cupom}">${data.porcentagem}%</p>
-            
                                             </div>
                                             <div class="area-excluir">
                                                 <a href="${data.qrCode}" download class='bx bx-down-arrow-circle' style='color:#2a55c2'></a>
-                                                <button onclick="excluircupom('${codigoCupom}','${data.pontos}')" class="btn-excluir">EXCLUIR</button>
+                                                <button onclick="excluircupom('${codigoCupom}', '${data.pontos}', this)" class="btn-excluir">EXCLUIR</button>
                                                 <p class="mensagem-cupom">Cupom prestes à expirar</p>
                                             </div>
-            
                                             <!-- Modal -->
                                             <div id="modal-excluir" class="modal-excluir">
                                                 <div class="modal-content">
@@ -39,9 +38,7 @@ function pegarCupons() {
                                                     <button id="cancelar-excluir" class="btn-cancelar">Cancelar</button>
                                                 </div>
                                             </div>`;
-              document
-                .querySelector(".lista-cupons")
-                .appendChild(containerCupom);
+              document.querySelector(".lista-cupons").appendChild(containerCupom);
             });
           } else {
             console.log("amigo tem não");
@@ -52,60 +49,70 @@ function pegarCupons() {
     }
   });
 }
+
 function transformarString(texto) {
-    // Converte para maiúsculas
-    let textoMaiusculo = texto.toUpperCase();
-    // Substitui '-' por espaço
-    let textoFormatado = textoMaiusculo.replace(/-/g, ' ');
-    return textoFormatado;
+  // Converte para maiúsculas
+  let textoMaiusculo = texto.toUpperCase();
+  // Substitui '-' por espaço
+  let textoFormatado = textoMaiusculo.replace(/-/g, ' ');
+  return textoFormatado;
 }
 
-function excluircupom(cupom, pontos) {
+function excluircupom(cupom, pontos, buttonElement) {
   firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-          const globalUserId = user.uid;
+    if (user) {
+      const globalUserId = user.uid;
 
-          // Remove o cupom
-          firebase
-              .database()
-              .ref("users/" + globalUserId + "/cupons/" + cupom)
-              .remove()
-              .then(() => {
-                  console.log('Cupom deletado com sucesso.');
-              })
-              .catch((erro) => {
-                  console.error('Erro ao deletar cupom:', erro);
-              });
+      // Remove o cupom
+      firebase
+        .database()
+        .ref("users/" + globalUserId + "/cupons/" + cupom)
+        .remove()
+        .then(() => {
+          console.log('Cupom deletado com sucesso.');
 
-          // Recupera os pontos do usuário
+          // Remove o elemento da tela
+          const liElement = buttonElement.closest(".conjunto-cupons"); // Encontra o elemento pai mais próximo
+          if (liElement) {
+            liElement.remove(); // Remove o elemento do DOM
+          }
+        })
+        .catch((erro) => {
+          console.error('Erro ao deletar cupom:', erro);
+        });
+
+      // Recupera os pontos do usuário
+      firebase
+        .database()
+        .ref("users/" + globalUserId)
+        .once('value')
+        .then((snapshot) => {
+          const pontosFirebase = snapshot.val().pontos;  
+          const pontosSalvos = Number(pontosFirebase); // Converte para número
+          
+          // Verifica se a conversão foi bem-sucedida
+          if (isNaN(pontosSalvos)) {
+            console.error('Pontos existentes não são um número válido.');
+            return; // Sai da função se a conversão falhar
+          }
+          
+          const pontosAtualizado = pontosSalvos + Number(pontos); // Converte 'pontos' para número
+          
+          // Atualiza os pontos no banco de dados
           firebase
-              .database()
-              .ref("users/" + globalUserId)
-              .once('value')
-              .then((snapshot) => {
-                  const pontosFirebase = snapshot.val().pontos;  
-                  const pontosSalvos = Number(pontosFirebase); // Converte para número
-                  // Verifica se a conversão foi bem-sucedida
-                  if (isNaN(pontosSalvos)) {
-                      console.error('Pontos existentes não são um número válido.');
-                      return; // Sai da função se a conversão falhar
-                  }
-                  const pontosAtualizado = pontosSalvos + Number(pontos); // Converte 'pontos' para número
-                  // Atualiza os pontos no banco de dados
-                  firebase
-                      .database()
-                      .ref("users/" + globalUserId)
-                      .update({ pontos: pontosAtualizado })
-                      .then(() => {
-                          console.log('Pontos atualizados:', pontosAtualizado);
-                      })
-                      .catch((error) => {
-                          console.error('Erro ao atualizar pontos:', error);
-                      });
-              })
-              .catch((error) => {
-                  console.error('Erro ao recuperar dados do usuário:', error);
-              });
-      }
+            .database()
+            .ref("users/" + globalUserId)
+            .update({ pontos: pontosAtualizado })
+            .then(() => {
+              console.log('Pontos atualizados:', pontosAtualizado);
+            })
+            .catch((error) => {
+              console.error('Erro ao atualizar pontos:', error);
+            });
+        })
+        .catch((error) => {
+          console.error('Erro ao recuperar dados do usuário:', error);
+        });
+    }
   });
 }
